2ebb24c121beb4542b56df45ce881c25b13db59c
Acrylic125 <acrylic125email@gmail.com>
2021-03-21 21:09:18 +0800

    PathTypeResultByHeightImpl.java

--- UniversalNMS/src/main/java/com/acrylic/universalnms/pathfinder/PathTypeResultByHeightImpl.java
+++ UniversalNMS/src/main/java/com/acrylic/universalnms/pathfinder/PathTypeResultByHeightImpl.java
@@ -3,6 +3,7 @@ package com.acrylic.universalnms.pathfinder;
 import com.acrylic.universal.utils.BitMaskUtils;
 import com.acrylic.universalnms.pathfinder.impl.PathWorldBlockReaderImpl;
 import com.acrylic.universalnms.worldexaminer.BoundingBoxExaminer;
+import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -66,24 +67,30 @@ public class PathTypeResultByHeightImpl implements PathTypeResult {
     }
 
     /**
+     *
+     * Contains:
      * 0x01 - Bypass (HIGHEST PRIORITY)
      * 0x02 - Swim (HIGH PRIORITY)
      * 0x04 - Climb (MEDIUM PRIORITY)
      * ...
      * 0x0 - None (LOWEST PRIORITY)
      *
+     * Scanner:
+     * 0x01 - Done start
+     * 0x02 - Done end
+     * 0x0 - None
+     *
      * @param pathExaminer The examiner.
      */
     @Override
     public void examineWith(@NotNull PathExaminer pathExaminer) {
         int floorMinY = (int) Math.floor(minYToScan),
-            ceilMaxY = (int) Math.ceil(maxYToScan),
-            arrIndex = 0;
+            ceilMaxY = (int) Math.ceil(maxYToScan);
         if (maxYToScan < minYToScan)
             throw new IllegalStateException("The max scan must be > than the min scan.");
-        float startingY, endingY,
-              checkpointStartY, checkpointEndY;
-        int contains = 0x0;
+        float startingY = minYToScan, endingY = maxYToScan,
+              checkpointStartY = minYToScan;
+        int contains = 0x0, scanner = 0x0;
         //The check state determines the status of scanning.
         //0 = Bottom most block, 1 = Seeking for top most block.
         byte checkState = 0;
@@ -95,9 +102,25 @@ public class PathTypeResultByHeightImpl implements PathTypeResult {
             if (pathType == null) {
                 BoundingBoxExaminer collisionBox = pathBlock.getCollisionBoundingBoxExaminer();
                 if (checkState == 0) {
-                    startingY = (float) collisionBox.getMaxY();
+                    if ((scanner & 0x01) == 0x01) {
+                        checkpointStartY = (float) collisionBox.getMaxY();
+                    } else {
+                        scanner |= 0x01;
+                        startingY = (float) collisionBox.getMaxY();
+                        checkpointStartY = startingY;
+                    }
                 } else {
-                    endingY = (float) collisionBox.getMinY();
+                    if ((scanner & 0x02) == 0x02) {
+                        float checkpointEndY = (float) collisionBox.getMinY();
+                        if ((checkpointEndY - checkpointStartY) > (endingY - startingY)) {
+                            startingY = checkpointStartY;
+                            endingY = checkpointEndY;
+                        }
+                    } else {
+                        scanner |= 0x02;
+                        endingY = (float) collisionBox.getMinY();
+                    }
+                    checkState = 0;
                 }
             } else {
                 checkState = 1;
@@ -110,7 +133,19 @@ public class PathTypeResultByHeightImpl implements PathTypeResult {
                         contains |= 0x04;
                 }
             }
-            arrIndex++;
+        }
+        if ((startingY - endingY) < heightRequirement) {
+            Bukkit.broadcastMessage("Invalid! " + heightRequirement + " is greater than " + (startingY - endingY));
+            return;
+        }
+        if ((contains & 0x01) == 0x01) {
+            this.pathType = PathType.BYPASS;
+        } else if ((contains & 0x02) == 0x02) {
+            this.pathType = PathType.SWIM;
+        } else if ((contains & 0x04) == 0x04) {
+            this.pathType = PathType.CLIMB;
+        } else {
+            this.pathType = PathType.WALK;
         }
     }
 
